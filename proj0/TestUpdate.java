754a2e2915ef746261abb3636a5128bd5dfceb00
Diether Delosreyes <dietdlsrys@gmail.com>
2019-01-24 09:37:05 -0800

    completed project over winter break

--- proj0/TestUpdate.java
+++ proj0/TestUpdate.java
@@ -1,7 +1,5 @@
-import java.math.*;
-
 /**
- *  Tests Planet's update() method
+ *  Tests Body's update() method
  */
 public class TestUpdate {
 
@@ -22,26 +20,28 @@ public class TestUpdate {
      */
     private static void checkEquals(double expected, double actual, String label, double eps) {
         if (Math.abs(expected - actual) <= eps * Math.max(expected, actual)) {
-            System.out.println("PASS: " + label + ": Expected " + expected + " and you gave " + actual);
+            System.out.println("PASS: " + label
+                    + ": Expected " + expected + " and you gave " + actual);
         } else {
-            System.out.println("FAIL: " + label + ": Expected " + expected + " and you gave " + actual);
+            System.out.println("FAIL: " + label
+                    + ": Expected " + expected + " and you gave " + actual);
         }
     }
 
 
     /**
-     *  Checks the Planet class to make sure update works.
+     *  Checks the Body class to make sure update works.
      */
     private static void checkUpdate() {
         System.out.println("Checking update...");
 
-        Planet p1 = new Planet(1.0, 1.0, 3.0, 4.0, 5.0, "jupiter.gif");
+        Body b1 = new Body(1.0, 1.0, 3.0, 4.0, 5.0, "jupiter.gif");
 
-        p1.update(2.0, 1.0, -0.5);
+        b1.update(2.0, 1.0, -0.5);
 
-        checkEquals(3.4, p1.xxVel, "xxVel update()", 0.01);
-        checkEquals(3.8, p1.yyVel, "yyVel update()", 0.01);
-        checkEquals(7.8, p1.xxPos, "xxPos update()", 0.01);
-        checkEquals(8.6, p1.yyPos, "yyPos update()", 0.01);
+        checkEquals(3.4, b1.xxVel, "xxVel update()", 0.01);
+        checkEquals(3.8, b1.yyVel, "yyVel update()", 0.01);
+        checkEquals(7.8, b1.xxPos, "xxPos update()", 0.01);
+        checkEquals(8.6, b1.yyPos, "yyPos update()", 0.01);
     }
 }
