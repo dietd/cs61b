7367766d1f8c1c56f4f8c6c867685453986f437f
Diether Delosreyes <dietdlsrys@gmail.com>
2019-02-28 19:56:22 -0800

    finished lab6

--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -1,34 +1,70 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class UnionFind {
 
-    // TODO - Add instance variables?
+    int[] parent;
+    int[] size;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int n) {
-        // TODO
+        parent = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i += 1) {
+            parent[n] = -1;
+            size[n] = 1;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
-        // TODO
+        if (vertex < 0 || vertex > parent.length - 1) {
+            throw new IllegalArgumentException();
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
-        // TODO
-        return -1;
+        validate(v1);
+        return size[v1];
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
-        // TODO
-        return -1;
+        validate(v1);
+        if (v1 == -1) {
+            return -size[v1];
+        }
+        return parent[v1];
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO
+        validate(v1);
+        validate(v2);
+        List<Integer> parentsv1 = new ArrayList<>();
+        List<Integer> parentsv2 = new ArrayList<>();
+        while (parent[v1] != -1) {
+            parentsv1.add(v1);
+            v1 = parent[v1];
+        }
+
+        while (parent[v2] != -1) {
+            parentsv2.add(v2);
+            v2 = parent[v2];
+        }
+
+        /* Checks if parentsv1 has item contained in parentsv2*/
+        for (int i : parentsv1) {
+            for (int j : parentsv2) {
+                if (i == j) {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 
@@ -38,14 +74,22 @@ public class UnionFind {
        vertex with itself or vertices that are already connected should not 
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
-        // TODO
+        if (sizeOf(v1) <= sizeOf(v2)) {
+            parent[find(v2)] = find(v1);
+        } else {
+            parent[find(v1)] = find(v2);
+        }
+        size[v1] += size[v2];
+        size[v2] += size[v1];
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int vertex) {
-        // TODO
-        return -1;
+        validate(vertex);
+        while (parent[vertex] != -1) {
+            vertex = parent[vertex];
+        }
+        return vertex;
     }
-
 }
