29d41bc48cee10084b10f8b20b5825532dada1b5
Diether Delosreyes <dietdlsrys@gmail.com>
2019-03-02 18:10:40 -0800

    need to fix backwash

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,4 +4,113 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
+    private int openSites = 0;
+    private int size;
+    private int N;
+    private WeightedQuickUnionUF gridUnions;
+    private boolean[][] grid;
+
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {
+
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.N = N;
+        size = N * N;
+        gridUnions = new WeightedQuickUnionUF(size + 2);
+        grid = new boolean[N][N];
+        for(int i = 0; i < N; i += 1) {
+            gridUnions.union(i, size);
+            gridUnions.union(size - 1 - i, size + 1);
+        }
+    }
+
+    //returns false if out of bounds
+    private boolean check(int num) {
+        if (num >= N || num < 0) {
+            return false;
+        }
+        return true;
+    }
+
+    //throws exception
+    private void outofBounds(int num) {
+        if (!check(num)) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col) {
+
+        outofBounds(row);
+        outofBounds(col);
+
+        if (!isOpen(row, col)) {
+            grid[row][col] = true;
+            openSites += 1;
+
+            if (check(row - 1)) {
+                if (isOpen(row - 1, col)) {
+                    gridUnions.union(row * N + col,(row - 1) * N + col);
+                }
+            }
+
+            if (check(row + 1)) {
+                if (isOpen(row + 1, col)) {
+                    gridUnions.union(row * N + col, (row + 1) * N + col);
+                }
+            }
+
+            if (check(col + 1)) {
+                if (isOpen(row, col + 1)) {
+                    gridUnions.union(row * N + col, row * N + col + 1);
+                }
+            }
+
+            if (check(col - 1)) {
+                if (isOpen(row, col - 1)) {
+                    gridUnions.union(row * N + col, row * N + col - 1);
+                }
+            }
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+
+        outofBounds(row);
+        outofBounds(col);
+
+        return grid[row][col];
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+
+        outofBounds(row);
+        outofBounds(col);
+
+        if (!gridUnions.connected(size, row * N + col)) {
+            return false;
+        }
+
+        return gridUnions.connected(size, row * N + col) && grid[row][col];
+    }
+
+    // number of open sites
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return gridUnions.connected(size, size + 1);
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,57 @@
 package hw2;
 
+import java.util.ArrayList;
+import java.util.List;
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
+
+    private double[] trials;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.trials = new double[T];
+
+        for (int t = 0; t < T; t += 1) {
+
+            Percolation p = pf.make(N);
+            List<Integer> blocked = new ArrayList<>();
+            for (int i = 0; i < (N * N); i += 1) {
+                blocked.add(i);
+            }
+
+            while (p.percolates()) {
+                int toOpen = StdRandom.uniform(blocked.size());
+                int row = Math.floorDiv(toOpen, N);
+                int col = Math.floorMod(toOpen, N);
+                p.open(row, col);
+                blocked.remove(toOpen);
+            }
+
+            trials[t] = (double) p.numberOfOpenSites() / (double) (N * N);
+
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(trials);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(trials);
+    }
+
+    public double confidenceLow() {
+        return mean() - ((1.96 * stddev()) / Math.pow((double) trials.length, .5));
+    }
+
+    public double confidenceHigh() {
+        return mean() + ((1.96 * stddev()) / Math.pow((double) trials.length, .5));
+    }
 
 }
